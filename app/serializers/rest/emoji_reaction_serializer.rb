4b432828d020d0fc49923214bd59b5bebdafda50
yutaka tsumori <shigure@refy.net>
2023-07-07 18:26:16 +0900

    Respond emoji_reaction field in the notification

---
+++ app/serializers/rest/emoji_reaction_serializer.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+class REST::EmojiReactionSerializer < ActiveModel::Serializer
+  include RoutingHelper
+
+  attributes :name, :count, :status_id, :account_ids, :me
+
+  attribute :url, if: :custom_emoji?
+  attribute :static_url, if: :custom_emoji?
+  attribute :domain, if: :custom_emoji?
+
+  def count
+    self.related_reaction.count
+  end
+
+  def me
+    self.related_reaction.me
+  end
+
+  def account_ids
+    [object.account_id.to_s]
+  end
+  
+  def url
+    full_asset_url(object.custom_emoji.image.url)
+  end
+  
+  def static_url
+    full_asset_url(object.custom_emoji.image.url(:static))
+  end
+  
+  def domain
+    object.custom_emoji.domain
+  end
+
+  def custom_emoji?
+    object.custom_emoji.present?
+  end
+
+  private
+  def related_reaction
+    return @related_reaction_cache if @related_reaction_cache.present?
+    @related_reaction_cache = object.status.reactions_hash(current_user&.account).find{|r|
+      (!self.custom_emoji? || r[:domain] == self.domain) && r[:name] == object.name
+    }
+  end
+end
